f278754b3b9852336e8d4a86861babb4558330cb
Priya Mattaparti <priyamattaparti@Priyas-MacBook-Air.local>
2025-02-22 16:40:41 +0530

    Adding a view

---
+++ src/db/views/EMP_DETAILS_VIEW.sql
@@ -0,0 +1,6 @@
+
+DBMS_METADATA.GET_DDL('VIEW','EMP_DETAILS_VIEW','HR')                              
+__________________________________________________________________________________ 
+
+  CREATE OR REPLACE FORCE EDITIONABLE VIEW "HR"."EMP_DETAILS_VIEW" ("EMPLOYEE_I    
+
